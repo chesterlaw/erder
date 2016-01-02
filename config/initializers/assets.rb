27da8921eeda4c698c08d0ba645e6cf1c3e96baf
Chester Law <chesterlaw@me.com>
2016-01-01 16:53:41 -0800

    Add lowercase-grid

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( uppercase-grid.png )
+Rails.application.config.assets.precompile += %w( lowercase-grid.png )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
