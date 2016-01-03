7c18f9132219f6f8d79b9de0ff47d8ff3ae5ca9e
Chester Law <chesterlaw@me.com>
2016-01-03 14:43:48 -0800

    Improve previous-and-next-button.js

--- app/assets/javascripts/previous-and-next-button.js
+++ app/assets/javascripts/previous-and-next-button.js
@@ -1,26 +1,39 @@
 $(document).ready(function() {
   var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var characters = alphabet.slice(0); // Clone alphabet.
+  (function add_lowercase_letters() {
+    for (var i = 0; i < alphabet.length; i += 1) {
+      characters.push('lowercase_' + alphabet[i]);
+    }
+  })();
 
   $('.previous-button').on('click', function() {
-    var here = window.location.pathname[1];
+    var here = window.location.pathname.slice(1);
     var origin = window.location.origin;
-    var hereIndex = alphabet.indexOf(here);
-    if (hereIndex == alphabet.length - 1) {
-      var previousPage = window.location.origin + '/' + alphabet[0];
+    var hereIndex = characters.indexOf(here);
+    characters.push()
+    if (hereIndex === 0) {
+      var previousPage = window.location.origin + '/' + characters[characters.length - 1];
+      characters.push()
     } else {
-      var previousPage = window.location.origin + '/' + alphabet[hereIndex - 1];
+      var previousPage = window.location.origin + '/' + characters[hereIndex - 1];
+      characters.push()
     }
     window.location.href = previousPage
   });
 
   $('.next-button').on('click', function() {
-    var here = window.location.pathname[1];
+    var here = window.location.pathname.slice(1);
     var origin = window.location.origin;
-    var hereIndex = alphabet.indexOf(here);
-    if (hereIndex == alphabet.length - 1) {
-      var nextPage = window.location.origin + '/' + alphabet[0];
+    var hereIndex = characters.indexOf(here);
+    characters.push()
+    if (hereIndex == characters.length - 1) {
+      characters.push()
+      var nextPage = window.location.origin + '/' + characters[0];
+      characters.push()
     } else {
-      var nextPage = window.location.origin + '/' + alphabet[hereIndex + 1];
+      var nextPage = window.location.origin + '/' + characters[hereIndex + 1];
+      characters.push()
     }
     window.location.href = nextPage
   });
