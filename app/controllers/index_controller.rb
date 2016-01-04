3c3016896ab071b97af6acfeb2019c8e28360682
Chester Law <chesterlaw@me.com>
2016-01-03 19:53:26 -0800

    Ruby to perform previous and next buttons

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,2 +1,20 @@
 class IndexController < ApplicationController
+  before_action :get_previous_and_next_character, except: :index
+
+  def get_previous_and_next_character
+    alphabet = ('a' .. 'z').to_a
+    characters = ('a' .. 'z').to_a
+
+    alphabet.each do |letter|
+      characters << 'lowercase_' + letter
+    end
+
+    hereIndex = characters.index(action_name)
+    @previousPage = characters[hereIndex - 1]
+    @nextPage = characters[hereIndex + 1]
+
+    if hereIndex == characters.count - 1
+      @nextPage = characters[0]
+    end
+  end
 end
