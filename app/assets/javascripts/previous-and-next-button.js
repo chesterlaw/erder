6d258f3b0b3a5ca67c78d9121b68135a9f1ac3c7
Chester Law <chesterlaw@me.com>
2016-01-03 19:47:23 -0800

    Clean previous-and-next-button.js

--- app/assets/javascripts/previous-and-next-button.js
+++ app/assets/javascripts/previous-and-next-button.js
@@ -11,13 +11,10 @@ $(document).ready(function() {
     var here = window.location.pathname.slice(1);
     var origin = window.location.origin;
     var hereIndex = characters.indexOf(here);
-    characters.push()
     if (hereIndex === 0) {
       var previousPage = window.location.origin + '/' + characters[characters.length - 1];
-      characters.push()
     } else {
       var previousPage = window.location.origin + '/' + characters[hereIndex - 1];
-      characters.push()
     }
     window.location.href = previousPage
   });
@@ -26,14 +23,11 @@ $(document).ready(function() {
     var here = window.location.pathname.slice(1);
     var origin = window.location.origin;
     var hereIndex = characters.indexOf(here);
-    characters.push()
     if (hereIndex == characters.length - 1) {
       characters.push()
       var nextPage = window.location.origin + '/' + characters[0];
-      characters.push()
     } else {
       var nextPage = window.location.origin + '/' + characters[hereIndex + 1];
-      characters.push()
     }
     window.location.href = nextPage
   });
